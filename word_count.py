fa65bdaa9b32e161126af25b8e0276b606a4027e
Fcontrvillaa <fcontreras+1@gmail.com>
2025-05-28 21:30:27 +0000

    okk

--- word_count.py
+++ word_count.py
@@ -7,14 +7,25 @@ archivo = sys.argv[1]
 with open(archivo, "r", encoding="utf-8") as f:
     #print(f.read())
     texto = f.read()
-
 #print (texto)
 
-total_letras = len(texto)
+# cuenta palabras
+total_palabras = texto.split()
+print (len(total_palabras))
+
+
+# cuenta letras
+total_letras = texto.replace(" ","")
+total_letras = texto.replace(".","")
+#total_letras = texto.lower
+print (len(total_letras))
+#total_letras = len(texto)
+
+#print(dir(texto))
 
 
 
 
-total_palabras = texto.split
-print(total_letras)
+#total_palabras = texto.split
+#print(total_letras)
 #print(f" {total_palabras}")
