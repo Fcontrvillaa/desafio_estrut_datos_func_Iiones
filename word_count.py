dec882e1ae552bb990a78fc46a99ccd96e9172c8
Fcontrvillaa <fcontreras+1@gmail.com>
2025-05-28 04:07:03 +0000

    leyendo archi

--- word_count.py
+++ word_count.py
@@ -1,2 +1,20 @@
 import sys
 
+archivo = sys.argv[1]
+
+
+#leer archivo
+with open(archivo, "r", encoding="utf-8") as f:
+    #print(f.read())
+    texto = f.read()
+
+#print (texto)
+
+total_letras = len(texto)
+
+
+
+
+total_palabras = texto.split
+print(total_letras)
+#print(f" {total_palabras}")
