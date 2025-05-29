f758dd2b0ce1df21a0b81476d7505f0ee3085717
Fcontrvillaa <fcontreras+1@gmail.com>
2025-05-29 21:00:57 +0000

    count ok

--- word_count.py
+++ word_count.py
@@ -2,30 +2,22 @@ import sys
 
 archivo = sys.argv[1]
 
-
 #leer archivo
 with open(archivo, "r", encoding="utf-8") as f:
     #print(f.read())
     texto = f.read()
 #print (texto)
 
-# cuenta palabras
+# cuenta caracteres distintos
+distintas_letras = set(texto)
+print (len(distintas_letras))
+
+# cuenta palabras distintas
 total_palabras = texto.split()
-print (len(total_palabras))
-
-
-# cuenta letras
-total_letras = texto.replace(" ","")
-total_letras = texto.replace(".","")
-#total_letras = texto.lower
-print (len(total_letras))
-#total_letras = len(texto)
-
-#print(dir(texto))
+distintas_palabras = set(total_palabras)
+print (len(distintas_palabras))
 
 
 
 
-#total_palabras = texto.split
-#print(total_letras)
-#print(f" {total_palabras}")
+
